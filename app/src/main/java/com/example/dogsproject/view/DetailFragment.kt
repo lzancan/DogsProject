751a6703c10e44b2f93d961030cdf59d85ef30db
Luciano Zancan Mazzutti <lmazzutti@nelogica.com.br>
2020-03-29 18:27:00 -0300

    Palette

--- app/src/main/java/com/example/dogsproject/view/DetailFragment.kt
+++ app/src/main/java/com/example/dogsproject/view/DetailFragment.kt
@@ -1,5 +1,7 @@
 package com.example.dogsproject.view
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,8 +10,13 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.example.dogsproject.R
 import com.example.dogsproject.databinding.FragmentDetailBinding
+import com.example.dogsproject.model.DogPalette
 import com.example.dogsproject.util.getProgressDrawable
 import com.example.dogsproject.util.loadImage
 import com.example.dogsproject.viewmodel.DetailViewModel
@@ -49,8 +56,26 @@ class DetailFragment : Fragment() {
         viewModel.dogLiveData.observe(viewLifecycleOwner, Observer { dog->
             dog?.let {
                 dataBinding.dog = dog
+                it.imageURL?.let {
+                    setupBackgroundColor(it)
+                }
             }
         })
     }
 
+    private fun setupBackgroundColor(url: String){
+        Glide.with(this).asBitmap().load(url).into(object: CustomTarget<Bitmap>(){
+            override fun onLoadCleared(placeholder: Drawable?) {
+            }
+
+            override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                Palette.from(resource).generate{palette ->
+                    val intColor = palette?.darkVibrantSwatch?.rgb?:0
+                    val myPalette = DogPalette(intColor)
+                    dataBinding.palette = myPalette
+                }
+            }
+
+        })
+    }
 }
